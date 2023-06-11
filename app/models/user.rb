fee66009b8e1feeeb2c9b41ea528df3a0d392e3c
talhanasir167 <talhanasir167@gmail.com>
2023-06-11 10:51:47 -0700

    Added model level validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :qualification, :experience, :description, :approved, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  ROLES = {
+    lawyer: 0,
+    admin: 1,
+    owner: 2
+  }.freeze
+
+  enum role: ROLES
 end
