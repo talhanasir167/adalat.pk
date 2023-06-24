35a4595f627308322ca20b3624451884b67e5996
talhanasir167 <talhanasir167@gmail.com>
2023-06-24 03:29:47 -0700

    Added confirmation for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   validates :first_name, :qualification, :experience, :description, :email, :province, :district,
             :tehsil_bar, :bar_concil_card, :id_card, presence: true
