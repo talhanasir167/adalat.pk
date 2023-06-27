0309dddaa60a16ad6cadcaf54473f01513ce882e
talhanasir167 <talhanasir167@gmail.com>
2023-06-27 11:15:34 -0700

    Updated Navbar UI and Seed data

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ApplicationRecord
-  validates :name, :avatar, presence: true
+  validates :name, presence: true
 
   has_one_attached :avatar
   has_many :categories, dependent: :destroy
