ef1e122a69145f55b0507e249d53d4b497b133a8
talhanasir167 <talhanasir167@gmail.com>
2023-07-11 17:46:41 -0700

    Updated services part on home page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,8 @@ module PagesHelper
   def set_navbar_user_url
     user_signed_in? && current_user.user_avatar || 'user_default_avatar.png'
   end
+
+  def add_style(element, style)
+    element.insert(-7, style)
+  end
 end
