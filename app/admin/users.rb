e910e243e27a8dc95a39e015584eeccfb953192b
talhanasir167 <talhanasir167@gmail.com>
2023-07-22 12:35:58 -0700

    Added user_image on active admin user show page

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -66,6 +66,7 @@ show_block = proc do
     attributes_table do
       row :name
       instance_eval(&user_summary_attributes) if user.user_summary.present?
+      row('User Profile') { |user| image_tag user.user_avatar, width: 100, height: 80 }
       row :services
       row :created_at
     end
