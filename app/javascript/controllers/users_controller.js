c9cb19f23326224af3f31748f539a81153e00f9a
talhanasir167 <talhanasir167@gmail.com>
2023-08-04 13:52:30 -0700

    Hot-fix

--- app/javascript/controllers/users_controller.js
+++ app/javascript/controllers/users_controller.js
@@ -1,19 +1,42 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = { type: String }
 
   read_more(event) {
     event.preventDefault();
     event.target.classList.add('d-none');
+    this.typeValue === 'description' ? this.read_more_description() : this.read_more_services();
+  }
+
+  read_less(event) {
+    event.preventDefault();
+    event.target.classList.add('d-none');
+    this.typeValue === 'description' ? this.read_less_description() : this.read_less_services();
+  }
+  
+  read_more_description() {
     let desElement = document.getElementById('short-description');
     desElement.classList.remove('read-less', 'p-3');
     desElement.classList.add('read-more', 'p-5');
     document.getElementById('read-less').classList.remove('d-none');
   }
 
-  read_less(event) {
-    event.preventDefault();
-    event.target.classList.add('d-none');
+  read_more_services() {
+    let desElement = document.getElementById('short-services');
+    desElement.classList.remove('read-less-services');
+    desElement.classList.add('read-more');
+    document.getElementById('read-less-services').classList.remove('d-none');
+  }
+
+  read_less_services() {
+    let desElement = document.getElementById('short-services');
+    desElement.classList.remove('read-more');
+    desElement.classList.add('read-less-services');
+    document.getElementById('read-more-services').classList.remove('d-none');
+  }
+
+  read_less_description() {
     let desElement = document.getElementById('short-description');
     desElement.classList.remove('read-more', 'p-5');
     desElement.classList.add('read-less', 'p-3');
