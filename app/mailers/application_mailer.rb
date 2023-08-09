c95cfde9a360f7993b405faa0064cad880847116
talhanasir167 <talhanasir167@gmail.com>
2023-08-09 16:02:39 -0700

    Updated admin email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
   if Rails.env.production?
-    default from: 'Adalat.pk <usman.zulfiqar065@gmail.com>'
+    default from: 'Adalat.pk <contact@adalat.pk>'
   else
-    default from: 'Adalat.pk Testing <usman.zulfiqar065@gmail.com>'
+    default from: 'Adalat.pk Testing <contact@adalat.pk>'
   end
   layout 'mailer'
 
   def admin_emails
-    ENV['LF_ADMIN_EMAILS'].try(:split) || ['usman.zulfiqar065@gmail.com']
+    ENV['LF_ADMIN_EMAILS'].try(:split) || ['contact@adalat.pk']
   end
 end
